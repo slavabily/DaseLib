7bd2e3f3410f60da2aa0aeb4f46a601c4fcc3f7a
slava bily <slavabily@me.com>
2023-04-13 10:47:42 +0300

    Initial project setup

---
+++ DaseLib/DaseLib.h
@@ -0,0 +1,18 @@
+//
+//  DaseLib.h
+//  DaseLib
+//
+//  Created by slava bily on 13.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DaseLib.
+FOUNDATION_EXPORT double DaseLibVersionNumber;
+
+//! Project version string for DaseLib.
+FOUNDATION_EXPORT const unsigned char DaseLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaseLib/PublicHeader.h>
+
+
---
+++ DaseLibExamples/DaseLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaseLibExamplesApp.swift
+//  DaseLibExamples
+//
+//  Created by slava bily on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DaseLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
